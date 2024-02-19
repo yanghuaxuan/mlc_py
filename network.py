7b9d60d00692c1f87ec4807ae1853965552a074c
hyang <hyang@hyang.xyz>
2024-02-18 16:27:50 -0800

    separate network and testing

--- network.py
+++ network.py
@@ -107,14 +107,3 @@ def SGD(net: Network, training_data: [tuple[[float], [float]]], mini_batch_size:
                 grad_w = [np.zeros(l.weights.shape) for l in net.layers]
                 c = 0
 
-def __main__():
-    ## AND Gate Example
-    # Create a Neural Network of input_shape 2
-    net0 = Network(input_shape=2, layers=[3,2, 1])
-#    print(backprop(net0, [1,1], [1]))
-    SGD(net0, training_data=[([1,1], [1]), ([1,0], [0]), ([0,1], [0]), ([0,0],[0])], mini_batch_size=2, epochs=10000, learn_rate=1e-3)
-    print(f"x: [1,1]: {forward(net0, [1,1])}")
-    print(f"x: [1,0]: {forward(net0, [1,0])}")
-    print(f"x: [0,1]: {forward(net0, [0,1])}")
-    print(f"x: [0,0]: {forward(net0, [0,0])}")
-__main__()
